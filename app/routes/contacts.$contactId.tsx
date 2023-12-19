dd65c8b7754713bda17fbcee134df2140a8b65e4
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-12-19 23:23:33 +0900

    Loading data

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,18 @@
-import { Form } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+
+import { getContact } from "../data";
 
 import type { ContactRecord } from "../data";
 
+export const loader = async ({ params }) => {
+  const contact = await getContact(params.contactId);
+  return json({ contact });
+};
+
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://avatars.githubusercontent.com/u/32073395",
-    twitter: "parsee1053",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
